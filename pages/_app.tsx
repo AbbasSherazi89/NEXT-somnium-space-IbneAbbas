f94c4a61275dc368b11b56120a7013b9ec167c56
ibne.abbassherazi89@gmail.com <ibne.abbassherazi89@gmail.com>
2022-12-02 18:00:14 +0500

    CDN link removed

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,10 @@
 import "../styles/globals.css";
-// import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 import type { AppProps } from "next/app";
-// import { useEffect } from "react";
+import { useEffect } from "react";
 export default function App({ Component, pageProps }: AppProps) {
-  // useEffect(() => {
-  //   require("bootstrap/dist/js/bootstrap.bundle.min.js");
-  // }, []);
+  useEffect(() => {
+    require("bootstrap/dist/js/bootstrap.bundle.min.js");
+  }, []);
   return <Component {...pageProps} />;
 }
