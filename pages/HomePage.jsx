6cc4daabbc6f55a370ea049062a27a9a5c5809d2
ibne.abbassherazi89@gmail.com <ibne.abbassherazi89@gmail.com>
2022-12-06 11:19:02 +0500

    changes applied

--- pages/HomePage.jsx
+++ pages/HomePage.jsx
@@ -1,14 +1,14 @@
 import React from "react";
-import Hero from "../Components/Elements/Hero";
-import AndroidPt from "../Components/Elements/AndroidPt";
-import Ethereum from "../Components/Elements/Ethereum";
-import Available from "../Components/Elements/Available";
-import VirtualReality from "../Components/Elements/VirtualReality";
-import InGame from "../Components/Elements/InGame";
-import Partners from "../Components/Elements/Partners";
-import FooterTop from "../Components/Elements/FooterTop";
-import Events from "../Components/Elements/Events";
-import Community from "../Components/Elements/Community";
+import Hero from "Components/Elements/Hero";
+import AndroidPt from "Components/Elements/AndroidPt";
+import Ethereum from "Components/Elements/Ethereum";
+import Available from "Components/Elements/Available";
+import VirtualReality from "Components/Elements/VirtualReality";
+import InGame from "Components/Elements/InGame";
+import Partners from "Components/Elements/Partners";
+import FooterTop from "Components/Elements/FooterTop";
+import Events from "Components/Elements/Events";
+import Community from "Components/Elements/Community";
 const HomePage = () => {
   return (
     <div>
